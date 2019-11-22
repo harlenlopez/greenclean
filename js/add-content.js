3865188ec90c44318ed26de5747f43553a7ac453
harlen lopez <harlen.d.lopez@gmail.com>
2019-11-21 19:37:05 -0800

    updeted js

--- js/add-content.js
+++ js/add-content.js
@@ -13,3 +13,43 @@ if (hourNow > 18) {
 }
 
 document.write('<h3>' + greeting + '</h3>' );
+
+
+var showOrder = function () {
+    var order  = prompt( 'what would you like to order');
+    var item ,  quantity;
+    var item = '';
+    
+    while (order !== 'washing' && order !== 'laundering'){
+     
+        order = prompt ( 'please do something different, select "washing" or "laundering"…');
+    };
+    
+    //ask for the quantity
+
+        quantity = prompt ( 'how many would you like');
+
+    //console.log(quantity);
+
+    //  while(isNaN(quantity) !== 'number'){
+    //      console.log(isNaN(quantity));
+    //      quantity = prompt ('PLEASE ENTER A NUMERICAL VALUE');
+    //  }
+
+    //validate the quantity 
+
+    if(order === 'washing'){
+    item = '<money_pile.jpg>';
+    
+    }else if ( order === 'laundering'){
+        item = '<money_picture.jpg>';
+    }
+    return item;
+    //A FOR LOOP
+
+    for(var i = 0; i <quantity; i = i + 1){
+        items = items + item;
+    }
+    return items;
+};
+showOrder();
